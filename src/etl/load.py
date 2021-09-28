665efb85bb1d970e3e7f033ed3b07b69197e02b0
yordan-marinov <jordanmarinov8@gmail.com>
2021-09-28 13:10:34 +0300

    Modified load.py by integated the generators and updated docx

--- src/etl/load.py
+++ src/etl/load.py
@@ -3,52 +3,60 @@
 import psycopg2
 from psycopg2.extras import execute_values
 from src import config
-from typing import List
 
 
-def from_json_values(data_list: List[dict]):
-    """Get the json values. Return nested list"""
+def from_json(data_gen):
+    """Get the json values. Return [[row]]"""
 
-    values = []
-    for current_dict in data_list:
+    all_rows = []
+    # for current_dict in data_gen:
+    for current_dict in data_gen:
+        # current_dict = next(data_gen)
         current_row = []
         for value in current_dict.values():
             # Parse values list into str
             if isinstance(value, list):
+                # To insert text not list
                 value = ','.join(map(str, value))
-            current_row.append(value)
-        values.append(current_row)
 
-    return values
+            current_row.append(value)
+
+        all_rows.append(current_row)
+
+    return all_rows
 
 
 # Insert json into postgresql.
 def connect_and_load(section, mod_data):
     db_config_params = config.db_config(section)
     try:
+        # Process info
         print(f'Connecting to database....')
+
+        # Context manager does the commit and close for us.
         with psycopg2.connect(**db_config_params) as connection:
             with connection.cursor() as cursor:
                 # Creat new table if does not exists.
                 cursor.execute(
                     "CREATE TABLE IF NOT EXISTS lager("
                     "id SERIAL PRIMARY KEY, "
-                    "name varchar (255), "
                     "abv REAL, "
-                    "first_brewed VARCHAR (255),"
+                    "name varchar (255), "
+                    "first_brewed DATE ,"
                     "food_pairing TEXT, "
                     "tagline TEXT);")
 
+                # Process info
+                print('Table was created successfully.')
+
                 # Insert into the table.
-                rows = from_json_values(mod_data)
-
                 sql_expression = 'INSERT INTO lager (abv, name, first_brewed, food_pairing, tagline) VALUES %s'
-
+                # Nested list to list due to execute_values()
+                rows = from_json(mod_data)
                 # execute_values preferred due to better performance
                 execute_values(cursor, sql_expression, rows)
 
-                connection.commit()
-
+                # Process info
                 print(f"Table 'lager' was created and populated successfully.")
 
     except (Exception, psycopg2.DatabaseError) as error:
